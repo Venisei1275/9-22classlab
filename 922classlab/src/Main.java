83b59f15e605cc9fcbac31888607b8644f0432ca
Venisei1275 <41522413+Venisei1275@users.noreply.github.com>
2022-09-22 19:26:07 -0400

    Add files via upload

---
+++ 922classlab/src/Main.java
@@ -0,0 +1,46 @@
+//Create a program that includes the following..
+
+//Receive 2 Integers and 1 String from the user Completed 
+
+//Use all 5 arithmetic operators (+, -, *, /, %) on the integers and print them to the console 
+
+//Use 5 String Methods on the string and print them to the console 
+
+//Add your code to a repository and submit your repository link.
+
+public class Main {
+	public static void main(String[] args) {
+		String name = "Yukio Da Don";
+		String a = "Im a city girl aint nothing free about me";
+		String str = "99! Problems Biggest One Is Me ";
+		String[] arrOfStr = str.split("", 1);
+		int i = 999; 	
+		int v = 777;
+		int e =	888;
+		int d = 222;
+		int c = 666;
+		int f = 555;
+		
+		System.out.println("Name:" + name);
+		System.out.println("Uppercase:" + name.toUpperCase());
+		System.out.println("Lowercase:" + name.toLowerCase());
+		for (String z : arrOfStr)
+         System.out.println(str);
+		System.out.println(name.endsWith("Yukio")); 
+		System.out.println(a);
+		System.out.println(i);
+		System.out.println(v);
+		System.out.println(i + v);
+		System.out.println(f - e);
+		System.out.println(i * v);
+		System.out.println(i / v);
+		System.out.println(i % v);
+		System.out.println("Uppercase:" + name.toUpperCase());
+		System.out.println("Lowercase:" + name.toLowerCase());
+	}
+}
+
+	
+	
+			
+	
